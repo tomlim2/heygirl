e3280d1d96253753d1ef3f3351fa51f77112078a
tomlim2 <tomlim@youns-MacBook-Pro.local>
2017-09-18 10:06:52 -0700

    Rergtret

--- bookmarklet-me.js
+++ bookmarklet-me.js
@@ -1,52 +1,69 @@
-javascript: function Gosling(i, g) {
-    this.ratio = i, this.imageurl = g
+function Gosling(ratio, imageurl){
+  this.ratio = ratio;
+  this.imageurl = imageurl;
 }
 
-function Randomize(i) {
-    return Math.floor(Math.random() * i.length)
+ var getGosling = {
+  init: function(myGosling){
+    this.myGosling = myGosling;
+  },
+
+  horizontal: function(){
+    return this.myGosling.filter(function(myGosling){
+      return myGosling.ratio === "horizontal";
+    });
+  },
+
+  vertical: function(){
+    return this.myGosling.filter(function(myGosling){
+      return myGosling.ratio === "vertical";
+    });
+  },
+
+  square: function (){
+    return this.myGosling.filter(function(myGosling){
+      return myGosling.ratio === "square";
+    });
+  }
+ };
+
+ function Randomize(images){
+    return Math.floor(Math.random() * images.length)
+ }
+
+var myGosling = [
+new Gosling("horizontal", "https://tomlim2.github.io/heygirl/sunset/horizontal1.jpg"),
+new Gosling("horizontal", "https://tomlim2.github.io/heygirl/sunset/horizontal2.jpeg"),
+
+new Gosling("vertical", "https://tomlim2.github.io/heygirl/sunset/vertical1.jpg"),
+new Gosling("vertical", "https://tomlim2.github.io/heygirl/sunset/vertical2.jpg"),
+
+new Gosling("square", "https://tomlim2.github.io/heygirl/sunset/sqaure1.jpg"),
+new Gosling("square", "https://tomlim2.github.io/heygirl/sunset/sqaure2.jpg"),
+
+
+function imageRatio(image) {
+  var proportion = image.height/image.width;
+
+  if(proportion > 1) {
+    return "vertical";
+  } else if(proportion === 1) {
+    return "square";
+  } else if(proportion < 1) {
+    return "horizontal";
+  }
 }
 
-function imageRatio(i) {
-    var g = i.height / i.width;
-    return g > 1 ? "vertical" : 1 === g ? "square" : 1 > g ? "horizontal" : void 0
-}
-var getGosling = {
-        init: function(i) {
-            this.myGosling = i
-        },
-        horizontal: function() {
-            return this.myGosling.filter(function(i) {
-                return "horizontal" === i.ratio
-            })
-        },
-        vertical: function() {
-            return this.myGosling.filter(function(i) {
-                return "vertical" === i.ratio
-            })
-        },
-        square: function() {
-            return this.myGosling.filter(function(i) {
-                return "square" === i.ratio
-            })
-        }
-    },
-    myGosling = [
-    new Gosling("horizontal", "https://tomlim2.github.io/heygirl/sunset/horizontal1.jpg"),
-    new Gosling("horizontal", "https://tomlim2.github.io/heygirl/sunset/horizontal2.jpeg"),
+(function (document) {
 
-    new Gosling("vertical", "https://tomlim2.github.io/heygirl/sunset/vertical1.jpg"),
-    new Gosling("vertical", "https://tomlim2.github.io/heygirl/sunset/vertical2.jpg"),
+  getGosling.init(myGosling);
+  var images = document.getElementsByTagName('img'), length = images.length
 
-    new Gosling("square", "https://tomlim2.github.io/heygirl/sunset/sqaure1.jpg")];
-    new Gosling("square", "https://tomlim2.github.io/heygirl/sunset/sqaure2.jpg")];
-    
+  for (var i = 0; i < length; i++) {
+    var ratio = imageRatio(images[i]);
+    var number = Randomize(getGosling[ratio]());
+    var img = getGosling[ratio]()[number];
+    images[i].src = img.imageurl
+  }
 
-! function(i) {
-    getGosling.init(myGosling);
-    for (var g = i.getElementsByTagName("img"), o = g.length, n = 0; o > n; n++) {
-        var l = imageRatio(g[n]),
-            t = Randomize(getGosling[l]()),
-            e = getGosling[l]()[t];
-        g[n].src = e.imageurl
-    }
-}(document);
+})(document);
